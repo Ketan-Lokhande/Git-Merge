bb1b4e239af6d6dedac651251a03b2a44053307c
unknown <ketanlokhande123@gmail.com>
2023-04-13 10:45:42 +0530

    S commit

--- src/main/java/com/git/Application.java
+++ src/main/java/com/git/Application.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
+            String name="Sunil";
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-
+      public void printName(){
+System.out.print(name);
+}
 }
